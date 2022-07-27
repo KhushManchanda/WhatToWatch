6e5b6479bbac25cd86304a69b6069e70d4561ef9
KhushManchanda <manchandakhush@gmail.com>
2022-07-28 00:47:53 +0530

    Test

---
+++ client/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+	return (
+		<div>
+			<h1>Hello</h1>
+			<select name="genre" id="genre">
+				<option value="sci-fi">Sci-Fi</option>
+				<option value="saab">Romance</option>
+			</select>
+		</div>
+	);
+}
+
+export default App;
