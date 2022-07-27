04770e97398f7cb4f9c57f34461d97a5eb3aafd0
2802AdityA <adityasingla.2802@gmail.com>
2022-07-28 02:33:27 +0530

    Update

--- server/server.js
+++ server/server.js
@@ -5,6 +5,6 @@ app.get("/api", (req, res) => {
 	res.json({ users: ["userOne", "userTwo", "userThree"] });
 });
 
-app.listen(3000, () => {
-	console.log("Server started");
+app.listen(5000, () => {
+	console.log("Server started on port 5000");
 });
