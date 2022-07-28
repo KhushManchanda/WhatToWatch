6dd69986ccbdd5fee9a227fd168c747ddcaf350f
2802AdityA <adityasingla.2802@gmail.com>
2022-07-29 03:24:55 +0530

    Variables Changed

--- movieapi/src/components/MovieCard.jsx
+++ movieapi/src/components/MovieCard.jsx
@@ -1,22 +1,21 @@
 import React from "react";
 
-const MovieCard = ({movie}) => {
-    const IMAGE_PATH = 'https://image.tmdb.org/t/p/w500' 
+const MovieCard = ({ movie }) => {
+	const IMAGE_PATH = "https://image.tmdb.org/t/p/w500";
 
-    function openWindow(props) {
-        console.log(props);
+	return (
+		<div className={"movie-card"}>
+			{movie.poster_path ? (
+				<img
+					className={"movie-cover"}
+					src={`${IMAGE_PATH}${movie.poster_path}`}
+					alt=""
+				/>
+			) : null}
 
-    }
-    return(
-        <div className={"movie-card"}>
-            {movie.poster_path ? <img onClick={openWindow(movie.title)}  className={"movie-cover"} src={`${IMAGE_PATH}${movie.poster_path}`} alt=""/>
-            :null
-    }
-
-            <h5>{movie.title}</h5>
-
-        </div>
-    );
+			<h5>{movie.title}</h5>
+		</div>
+	);
 };
 
 export default MovieCard;
